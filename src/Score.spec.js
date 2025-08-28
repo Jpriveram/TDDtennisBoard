7a1ca197815bfcfc7895eb3e0526dd7a3ababa37
Juan Pablo <jpriveram01@gmail.com>
2025-08-28 09:47:39 -0400

    feat: Muestra cuando jugador 1 anota 1 punto

--- src/Score.spec.js
+++ src/Score.spec.js
@@ -2,7 +2,11 @@ import showScore from "./tennisScore";
 
 describe("Tennis Score", () => {
   it("game empieza con ambos en cero", () => {
-    expect(showScore()).toEqual("Love - Love");
+    expect(showScore(0)).toEqual("Love - Love");
+  });
+
+  it("Jugador 1 hace un punto", () => {
+    expect(showScore(15)).toEqual("15 - Love");
   });
 });
 
--- src/tennisScore.js
+++ src/tennisScore.js
@@ -1,5 +1,9 @@
-function showScore() {
+function showScore(J1) {
+  if (J1 === 0 )
   return "Love - Love";
+
+  if (J1 === 15 )
+  return "15 - Love";
 }
 
 export default showScore;
